d5c1eae5247e50a133d077eea27ee3adf410f873
ShoevRustam <67133591+ShoevRustam@users.noreply.github.com>
2021-01-02 07:03:33 +0200

    ConsoleApp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World1!");
+        }
+    }
+}
